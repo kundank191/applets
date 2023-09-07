f91cd28646debf68fc59e4411f586db31248ba17
Kundan <kundank191@gmail.com>
2023-09-07 21:01:16 +0530

    add inital code

---
+++ unacademy_scraper/ua_consts.py
@@ -0,0 +1,2 @@
+UA_BASE_URL = "https://unacademy.com/"
+COURSE_LINK_INTRUMENTATION_AND_PROCESS_CONTROL = "https://unacademy.com/course/complete-course-on-instrumentation-and-process-control-802/78EVGQNW"
